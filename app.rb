8a68ada6f151125a395348c3f5c9c376aed3d579
iainaitken <iain_aitken@yahoo.co.uk>
2021-02-15 16:31:12 +0000

    refactor code to include index.erb file

--- app.rb
+++ app.rb
@@ -11,7 +11,5 @@ get '/secret' do
 end
 
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-    <img src='https://images.pexels.com/photos/5004611/pexels-photo-5004611.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'>
-  </div>"
+  erb(:index)
 end
