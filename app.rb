ae0695d83c8a2dda28a515fa443790eb037e71ca
iainaitken <iain_aitken@yahoo.co.uk>
2021-02-15 16:20:44 +0000

    update with new /secret and /cat routes

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
   "Hello World"
+end
+
+get '/secret' do
+  "BadgerBadgerBadgerMushroomMushroom"
+end
+
+get '/cat' do
+  "<div>
+    <img src='https://images.pexels.com/photos/5004611/pexels-photo-5004611.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'>
+  </div>"
 end
